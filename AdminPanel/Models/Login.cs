1731de78bf51a1ad7a4b664d7e0779739aaf06df
死 <shinobiteuy1@gmail.com>
2019-07-22 16:59:07 +0700

    Auto stash before merge of "AdminPanel" and "origin/AdminPanel"
    aasdasd

---
+++ AdminPanel/Models/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace AdminPanel.Models
+{
+    public class Login
+    {
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+    }
+}
---
+++ AdminPanel/Views/Home/Login.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+namespace AdminPanel.Views.Home
+{
+    public class Login
+    {
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+            [Required]
+            public string Password { get; set; }
+            [Required]
+            public string Phonenumber { get; set; }
+
+        }
+    }
+}
